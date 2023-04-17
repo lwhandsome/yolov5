ffa9cb9a3d03fb83ee7d4668a5014ce3b69dfcff
林文涵 <75560822+lwhandsome@users.noreply.github.com>
2023-04-17 15:41:39 +0800

    11

--- film_cut.py
+++ film_cut.py
@@ -2,7 +2,7 @@ import cv2
 import os
 
     
-vc = cv2.VideoCapture(r'C:\Users\lwhan\Desktop\vedio2.mp4')  # 读入视频文件，命名cv
+vc = cv2.VideoCapture(r'C:\Users\lwhan\Desktop\vedio3.mp4')  # 读入视频文件，命名cv
 n = 1  # 计数
  
 if vc.isOpened():  # 判断是否正常打开
@@ -19,7 +19,7 @@ while rval:  # 循环读取视频帧
     if (n % timeF == 0):  # 每隔timeF帧进行存储操作
         i += 1
         print(i)
-        cv2.imwrite(r'C:\Users\lwhan\Desktop\data\s2/{}.jpg'.format(i), frame)  # 存储为图像
+        cv2.imwrite(r'C:\Users\lwhan\Desktop\data\s3/{}.jpg'.format(i), frame)  # 存储为图像
     n = n + 1
     cv2.waitKey(1)
 vc.release()
